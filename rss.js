bc4021550eda879a3f861d250f1fd7edac0760a0
Jaron Degen <jaron.degen90@gmail.com>
2020-08-21 05:40:37 +0200

    cleaned up news feed

--- rss.js
+++ rss.js
@@ -1,21 +1,5 @@
-// window.addEventListener(DOMContentLoaded, (e) => {
 
 
-    const feedUrl = `https://www.pokernews.com/rss.php`;
-
-    // fetch(feedUrl, { mode: 'no-cors'}).then((res) => {
-    //     console.log(res)
-    //     res.text().then((xmlTxt) => {
-    //       var domParser = new DOMParser()
-    //       let doc = domParser.parseFromString(xmlTxt, 'text/xml')
-    //       console.log(xmlTxt)
-    //       doc.querySelectorAll('item').forEach((item) => {
-    //          let h1 = document.createElement('h1')
-    //           h1.textContent = item.querySelector('title').textContent
-    //            document.querySelector('output').appendChild(h1)
-    //          })
-    //        })
-    //   })
     var url = 'https://www.pokernews.com/rss.php';
     
     const newsDiv = document.querySelector(".main__news")
@@ -27,16 +11,19 @@
 			var entries = result.feed.entries;
 			for(var i = 0; i < entries.length; i++){
                 var entry = entries[i];
-                let sum = entry.summary;
+                let entryLink = entry.link
+                let newLink = document.createElement("a")
+                newLink.setAttribute("href", entryLink)
+                newLink.setAttribute("class", "main__news-links")
+                newLink.innerHTML = entry.title
+                console.log(newLink)
+                let sum = entry.description;
                 let newSum = `${sum.slice(0,5)} class="rss_pic" ${sum.slice(5)}`
                 newsDiv.innerHTML += newSum;
-				// console.dir(entry);
+                newsDiv.appendChild(newLink)
+				console.dir(entry);
 			}
 		}
 	});
-	
 
-
-
-// })
 
