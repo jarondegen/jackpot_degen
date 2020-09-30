bcd9a2634493f8756764e792e828f8430a59bab2
Jaron Degen <jaron.degen90@gmail.com>
2020-09-30 19:39:49 +0200

    clean up

--- client/src/components/NewsFeed.js
+++ client/src/components/NewsFeed.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getFeed } from '../store/News';
@@ -7,23 +7,33 @@ import { getFeed } from '../store/News';
 const NewsFeed = () => {
     const dispatch = useDispatch();
     const { feed, imgs } = useSelector(state => state.News)
+    const [isLoading, setIsLoading] = useState(true)
     
     useEffect(() => {
         dispatch(getFeed())
-    },[])
+        setIsLoading(false)
+    },[]);
+
+    if (isLoading) {
+        return (
+            <div>NEWS LOADING</div>
+        );
+
+    };
 
     return (
         <>
-            <div>
+            <div>NEWS</div>
+            <div className="news-feed">
                 {feed && feed.map((item, i) => 
-                <>
-                    <div key={item.content}>
-                        <img key={imgs[i]}src={imgs[i]}/>
-                    </div>
-                    <div key={item.title}>
-                    <Link to={item.link} key={item.link}>{item.title}</Link>
-                    </div>
-                </>    
+                    <>
+                        <div key={item.content}>
+                            <img key={imgs[i]}src={imgs[i]}/>
+                        </div>
+                        <div key={item.title}>
+                        <Link to={item.link} key={item.link}>{item.title}</Link>
+                        </div>
+                    </>    
                 )}
             </div>
         </>
