b9a57dda4115f0be43577975e8093c637a7a1a2e
Jaron Degen <jaron.degen90@gmail.com>
2020-12-30 13:07:19 +0100

    invoke news feed

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -80,7 +80,7 @@ const Dashboard = () => {
                         <h2 className="big-jackpot-header">Biggest Current Jackpot</h2>
                         <BiggestCurrentJackpot />
                     </div>
-                    {/* <NewsFeed /> */}
+                    <NewsFeed />
                 </div>
             </div>
         </>
