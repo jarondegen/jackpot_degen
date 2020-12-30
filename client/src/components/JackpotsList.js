014fa1b69a964a73eb28e4ee3a1d22a6fc075009
Jaron Degen <jaron.degen90@gmail.com>
2020-12-30 13:03:57 +0100

    improve dashboard jackpot list

--- client/src/components/JackpotsList.js
+++ client/src/components/JackpotsList.js
@@ -9,16 +9,20 @@ const JackpotList = ({roomNames, jackpots, handleJackpotClick, sparkNumbers}) =>
             const isHitClass = jackpots[i] && jackpots[i].hit ? "jackpot-el-hit" : "jackpot-el-not-hit"
             return (
                 <div key={jackpots[i].roomId} id={jackpots[i].roomId} className={`my-jackpots-el-div ${isHitClass}`} onClick={handleJackpotClick}>
-                    <p className="my-jackports-room-name" id={jackpots[i].roomId}>{room}</p>
-                    <p className="my-jackports-room-hit" id={jackpots[i].roomId}>{isJackpotHit}</p>
-                    <div>
-                        <Sparklines data={sparkNumbers[i]} svgWidth={100} svgHeight={30}>
+                    <div onClick={handleJackpotClick} id={jackpots[i].roomId} className="my-jackports-room-name-div">
+                        <p className="my-jackports-room-name" id={jackpots[i].roomId}>{room}</p>
+                    </div>
+                    <div id={jackpots[i].roomId} className="jackpot-sparkline-div" onClick={handleJackpotClick}>
+                        <Sparklines id={jackpots[i].roomId} onClick={handleJackpotClick} data={sparkNumbers[i]} svgWidth={100} svgHeight={30}>
                             <SparklinesLine style={{stroke:jackpots[i].hit ? "red" : "green", strokeWidth:3, fill:jackpots[i].hit ? "red" : "green"}}  />
                         </Sparklines>
                     </div>
+                    <div id={jackpots[i].roomId} onClick={handleJackpotClick} className="jp-amount-and-hit-div">
                     <p className="my-jackports-room-amount" id={jackpots[i].roomId}>
                         {jackpots[i] && `$${jackpots[i].amount}`}
                     </p>
+                    <p className="my-jackports-room-hit" id={jackpots[i].roomId}>{isJackpotHit}</p>
+                    </div>
                 </div>
             )
         })
