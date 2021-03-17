d81395b62851f3f11e5e7759094d9dc78e8dd7a8
Jaron Degen <jaron.degen90@gmail.com>
2021-03-16 23:40:48 -0700

    undo append to index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST + '?sslmode=require',
+    host: process.env.DB_HOST,
   },
     jwtConfig: {
     secret: process.env.JWT_SECRET,
